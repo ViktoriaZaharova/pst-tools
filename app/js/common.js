5aef73b2b52166ea1c6e1daaf282d29aff9a9a37
Viktoria <zaharova181114@gmail.com>
2022-01-13 21:54:10 +0200

    add page + block

--- app/js/common.js
+++ app/js/common.js
@@ -9,4 +9,36 @@ $('.btn-close').on('click', function () {
     $('.mobile-menu').fadeOut();
 });
 
+$('.product-gallery-max').slick({
+    slidesToShow: 1,
+    fade: true,
+    asNavFor: '.product-gallery-preview',
+    arrows: false
+});
+
+$('.product-gallery-preview').slick({
+    slidesToShow: 4,
+    asNavFor: '.product-gallery-max',
+    focusOnSelect: true,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    responsive: [
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 420,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+
+
+    ]
+});
+
 new WOW().init();
+
